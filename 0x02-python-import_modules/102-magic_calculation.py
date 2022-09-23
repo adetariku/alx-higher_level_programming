210ef70aedc1d0d644aeee3b84a5f3a2f75b582f
Tariku Wendimagegn <adetariku@gmail.com>
2022-09-23 02:54:47 +0000

    fix error

--- 0x02-python-import_modules/102-magic_calculation.py
+++ 0x02-python-import_modules/102-magic_calculation.py
@@ -1,3 +1,10 @@
 #!/usr/bin/python3
-import string
-print(string.ascii_uppercase)
+def magic_calculation(a, b):
+	from calculator_1 import add, sub
+	if b > a: 
+		c = add(a,b)
+		for i in range(4, 6):
+			c = add(c,i)
+		return c
+	else:
+		return (sub(a,b))
