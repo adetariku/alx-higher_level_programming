c9e083bfc988627e7857659666acfda9cdd410bd
Tariku Wendimagegn <adetariku@gmail.com>
2022-09-26 15:24:27 +0000

    replace in copy

---
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+def new_in_list(my_list, idx, element):
+    copy_list = my_list[:]
+    if 0 <= idx < len(copy_list):
+        copy_list[idx] = element
+        return (copy_list)
+    return (my_list)
