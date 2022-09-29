a2b56dcb5b5fb91da81055e52f4be45c159cbcf1
Tariku Wendimagegn <adetariku@gmail.com>
2022-09-29 02:21:00 +0000

    return the gkey with the highest score

---
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+def best_score(a_dictionary):
+    if not a_dictionary:
+        return None
+    high_score = 0
+    for key, value in a_dictionary.items():
+        if value > high_score:
+            high_score = value
+    return high_score
