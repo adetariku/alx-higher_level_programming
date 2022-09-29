9b2253bbaab201c75f353090210467de01fd2f03
Tariku Wendimagegn <adetariku@gmail.com>
2022-09-29 02:29:34 +0000

    space corrected

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -3,7 +3,7 @@ def best_score(a_dictionary):
     if not a_dictionary:
         return None
     high_score = 0
-    high_key=""
+    high_key = ''
     for key, value in a_dictionary.items():
         if value > high_score:
             high_score = value
