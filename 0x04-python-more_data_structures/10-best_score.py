baca8b058755763de2ef46cd229a01a8eb9908b5
Tariku Wendimagegn <adetariku@gmail.com>
2022-09-29 02:27:16 +0000

    return key instead of score

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -3,7 +3,9 @@ def best_score(a_dictionary):
     if not a_dictionary:
         return None
     high_score = 0
+    high_key=""
     for key, value in a_dictionary.items():
         if value > high_score:
             high_score = value
-    return high_score
+            high_key = key
+    return high_key
