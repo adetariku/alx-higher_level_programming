e56959adf6618dabd432741bc2eb186f36fecb60
Tariku Wendimagegn <adetariku@gmail.com>
2022-10-18 02:47:17 +0000

    updated

--- 0x08-python-more_classes/6-rectangle.py
+++ 0x08-python-more_classes/6-rectangle.py
@@ -9,8 +9,8 @@ class Rectangle:
 
     number_of_instances = 0
 
-    def __init__(self, width=0, height=0):
-        """Thist method used to create new classes.
+    def __init__(self, width = 0, height = 0):
+        """Thist method used  to create new classes.
 	Initializes attributes of the rectangle"""
         self.width = width
         self.height = height
