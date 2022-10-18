09c9b2745e69ee61324ede9b1f7cdec7eef1f2c8
dragontww <adetariku@gmail.com>
2022-10-18 10:13:35 +0900

    creating a rectangle class.

---
+++ 0x08-python-more_classes/1-rectangle.py
@@ -0,0 +1,36 @@
+#!/usr/bin/python3
+"""
+This is a "Rectangle"  module that provides a simple Rectangle reprentation with two attributes. 
+These attributes are the width and the height. Default values of both attributes are 0.
+"""
+
+
+class Rectangle:
+    """A Rectangle class with setter and property mehods"""
+    def __init__(self, width=0, height=0):
+        self.width = width
+        self.height = height
+
+    @property
+    def width(self):
+        return self.__width
+
+    @width.setter
+    def width(self, value):
+        if type(value) is not int:
+            raise TypeError('width must be an integer')
+        if value < 0:
+            raise ValueError('width must be >= 0')
+        self.__width = value
+
+    @property
+    def height(self):
+        return self.__height
+
+    @height.setter
+    def height(self, value):
+        if type(value) is not int:
+            raise TypeError('height must be an integer')
+        if value < 0:
+            raise ValueError('height must be >= 0')
+        self.__height = value
