804d6ce1775ff73e002dad43bd26d8672865edd8
Tariku Wendimagegn <adetariku@gmail.com>
2022-10-23 02:34:54 +0000

    update

--- 0x07-python-test_driven_development/0-add_integer.py
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -1,5 +1,11 @@
 #!/usr/bin/python3
-"""This function adds integers."""
+"""
+add_integer:
+    Checks if parameters are int
+    Returns sum of parameters
+"""
+
+
 def add_integer(a, b=98):
     """adding integers. Throws exception if there is an error on type"""
     if not isinstance(a, int) and not isinstance(a, float):
