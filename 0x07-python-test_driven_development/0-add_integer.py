9bae686b677bc287827cdccb62a82f379c2f67c3
Tariku Wendimagegn <adetariku@gmail.com>
2022-10-23 02:26:41 +0000

    adding integers

--- 0x07-python-test_driven_development/0-add_integer.py
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
+"""This function adds integers."""
 def add_integer(a, b=98):
+    """adding integers. Throws exception if there is an error on type"""
     if not isinstance(a, int) and not isinstance(a, float):
         raise TypeError("a must be an integer")
     if not isinstance(b, int) and not isinstance(b, float):
