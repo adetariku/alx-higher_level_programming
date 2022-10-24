6930ec88aabc81b8e63887fb42333461ad63120a
Tariku Wendimagegn <adetariku@gmail.com>
2022-10-24 17:32:26 +0000

    update

--- 0x0A-python-inheritance/4-inherits_from.py
+++ 0x0A-python-inheritance/4-inherits_from.py
@@ -1,7 +1,11 @@
 #!/usr/bin/python3
+"""
+  inheritance in python
+"""
 
 
 def inherits_from(obj, a_class):
+    """inheritance """
     if type(obj) is not a_class and isinstance(obj, a_class):
         return True
     else:
