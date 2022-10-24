260c4e1787401ebef02d0086ad6bfbb53efe09f8
Tariku Wendimagegn <adetariku@gmail.com>
2022-10-24 17:21:56 +0000

    fix docs

--- 0x0A-python-inheritance/3-main.py
+++ 0x0A-python-inheritance/3-main.py
@@ -1,8 +1,10 @@
 #!/usr/bin/python3
+is_kind_of_class = __import__('3-is_kind_of_class').is_kind_of_class
 
-
-def is_kind_of_class(obj, a_class):
-    if isinstance(obj, a_class):
-        return True
-    else:
-        return False
+a = 1
+if is_kind_of_class(a, int):
+    print("{} comes from {}".format(a, int.__name__))
+if is_kind_of_class(a, float):
+    print("{} comes from {}".format(a, float.__name__))
+if is_kind_of_class(a, object):
+    print("{} comes from {}".format(a, object.__name__))
