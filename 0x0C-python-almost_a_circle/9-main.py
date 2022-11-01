9a36052a1b67b74b3c1f434d93a2038eda30b46e
Tariku Wendimagegn <adetariku@gmail.com>
2022-11-01 10:20:45 -0700

    unit testing square

--- 0x0C-python-almost_a_circle/9-main.py
+++ 0x0C-python-almost_a_circle/9-main.py
@@ -2,6 +2,7 @@
 """ 9-main """
 from models.square import Square
 
+
 if __name__ == "__main__":
 
     s1 = Square(5)
--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -1,15 +1,52 @@
-!#/usr/bin/python3
+#!/usr/bin/python3
 """square"""
-from rectangle import Rectangle 
+from  models.rectangle import Rectangle 
 
 
 class Square(Rectangle):
     def __init__(self, size, x=0, y=0, id=None):
-        super().__init__(def __init__(size, x, y, id)
-    
+        super().__init__(size, size, x, y, id)
+
+    @property
+    def size(self):
+        """size getter"""
+        return self.width
+
+    @size.setter
+    def size(self, value):
+        """size setter"""
+        self.width = value
+        self.height = value
+
+    def update(self, *args, **kwargs):
+        """updates the square??? i think"""
+        ind = 0
+        if args is not None and len(args) != 0:
+            for argument in args:
+                ind += 1
+                if ind == 1:
+                    self.id = argument
+                elif ind == 2:
+                    self.size = argument
+                elif ind == 3:
+                    self.x = argument
+                elif ind == 4:
+                    self.y = argument
+        else:
+            for key, values in kwargs.items():
+                setattr(self, key, values)
+
+    def to_dictionary(self):
+        """makes a self dictionary"""
+        dictionary = {}
+        for ind in ["id", "size", "x", "y"]:
+            dictionary[ind] = getattr(self, ind)
+        return dictionary
+
     def __str__(self):
-        """returns  shape information"""
-        return "[Square] ({:d}) {:d}/{:d} - {:d}".format(self.id,
-                                                         self.x,
-                                                         self.y,
-                                                         self.height)
+        """returns information about the shape"""
+        return "[Square] ({:d}) {:d}/{:d} - {:d}".format(
+                self.id,                                                                        self.x,
+                self.y,
+                self.height
+                )
